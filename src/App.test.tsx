eda9832c3e823c11d47da33334989fd80c732fb3
Maxwell Bruckhaus <maxwell.bruckhaus@gmail.com>
2022-02-04 15:16:10 -0800

    Update test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import Home from './Home';
 
 test('renders heading text', () => {
   render(<Home />);
-  const textElement = screen.getByText(/Home/i);
+  const textElement = screen.getByText('Maxwell Bruckhaus');
   expect(textElement).toBeInTheDocument();
 });
